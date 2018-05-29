bc83850904357419efb02b46a494a02bc24f3396
jlado <jose.luis.lado@gmail.com>
2018-05-29 09:59:46 +0200

    New version

--- download_latest.py
+++ download_latest.py
@@ -10,7 +10,15 @@ fname = "qh_temporal" # temporal folder for quantum honeycomp
 os.system("rm -rf "+fname) # remove temporal folder
 os.system("mkdir "+fname) # create temporal folder
 os.chdir(fname) # go to the temporal folder
-os.system("wget "+path) # download latest version from git
+
+import platform
+if platform.system()=="Linux":
+  os.system("wget "+path) # download latest version from git
+else: # for Mac
+  os.system("curl -LO "+path) # download latest version from git
+
+
+
 os.system("tar -xvf "+name) # untar file
 fols = next(os.walk('.'))[1] # get the directory
 if len(fols)>1:
