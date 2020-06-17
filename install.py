cc1cdcb593e364ef1041cd429004a0ab261bb7a3
joselado <jose.luis.lado@gmail.com>
2020-06-17 13:11:21 +0300

    Update

--- install.py
+++ install.py
@@ -7,8 +7,8 @@ import sys
 
 
 path = os.path.dirname(os.path.realpath(__file__)) # current path
-sys.path.append(path+"/pysrc/interpreter") # add the interpreter
-import pycommand
+sys.path.append(path+"/pysrc/") # add the interpreter
+from interpreter import pycommand
 
 pycommand.install_python() # install the correct python dist
 pycommand.install_dependencies() # install the dependencies
