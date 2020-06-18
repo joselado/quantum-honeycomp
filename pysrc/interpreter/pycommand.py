4d7069817e268d6691b052609e227e5e3b053375
joselado <jose.luis.lado@gmail.com>
2020-06-18 13:19:51 +0300

    Update

--- pysrc/interpreter/pycommand.py
+++ pysrc/interpreter/pycommand.py
@@ -88,7 +88,8 @@ def add_to_path():
     qhpath = os.path.dirname(os.path.realpath(__file__))+"/../../bin"
     try: ls = open(rcfile,"r").read() # if the file exists
     except: ls = "" # otherwise
-    addrc = "\nexport PATH=\""+qhpath+"\":$PATH\n"
+    addrc = "alias quantum-honeycomp=\"" + get_python() +" "+qhpath+"/quantum-honeycomp\""
+#    addrc = "\nexport PATH=\""+qhpath+"\":$PATH\n"
     open(rcfile,"w").write(ls+addrc) # add to the bash
 
 
