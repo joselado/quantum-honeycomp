eeabeea0e337e2872a3fb8ac5d8c09abeba6c87f
joselado <jose.luis.lado@gmail.com>
2020-06-18 18:39:17 +0300

    Update

--- install.py
+++ install.py
@@ -1,14 +1,8 @@
-
-# this script installs the quantum honeycomp package
 import os
 import sys
 
-
-path = os.path.dirname(os.path.realpath(__file__)) # current path
-sys.path.append(path+"/pysrc/") # add the interpreter
-from interpreter import pycommand
+from pysrc.interpreter import pycommand
 
 pycommand.install_python() # install the correct python dist
 pycommand.install_dependencies() # install the dependencies
 pycommand.add_to_path() # install the correct python dist
-
