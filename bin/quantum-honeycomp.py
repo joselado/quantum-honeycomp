5f6b7a68ec8f6c118869d50df50e3a600a2a5ffe
Benjamin Alldritt <benjamin.alldritt@aalto.fi>
2020-07-02 12:10:10 +0300

    Fix path and script execution

--- bin/quantum-honeycomp.py
+++ bin/quantum-honeycomp.py
@@ -1,51 +1,55 @@
 import os
 import sys
 import platform
-
-
-
 import argparse
+import subprocess
+from pathlib import Path
+
+
 parser = argparse.ArgumentParser()
-parser.add_argument("--utility",default="",
-        help='Just use an utility and exit')
-parser.add_argument("--python",default="",
-        help='Default Python interpreter')
-args = parser.parse_args() # get the arguments
-
-
+parser.add_argument("--utility", default="",
+                    help='Just use a utility and exit')
+parser.add_argument("--python", default="",
+                    help='Default Python interpreter')
+args = parser.parse_args()  # get the arguments
 
 
 def get_qhroot():
-  """Gets the root path of quantum honeycomp"""
-  return os.path.dirname(os.path.dirname(os.path.realpath(__file__))) 
-  #return os.path.dirname(os.path.realpath(__file__))+"/../" 
+    """Gets the root path of quantum honeycomp"""
+    return Path(__file__).resolve().parent.parent
+
 
 qhroot = get_qhroot()  # get the root path
-os.environ["QHROOT"] = qhroot # create the environmental variable
+os.environ["QHROOT"] = str(qhroot)  # create the environmental variable
 
 
 def get_command(name="python"):
-  """Return the path for Anaconda Python, which has pyqt by default"""
-  dirname = os.path.dirname(os.path.realpath(__file__))
-  sys.path.append(dirname+"/../pysrc/") # add this path
-  from interpreter import pycommand
-  return pycommand.get_python()
+    """Return the path for Anaconda Python, which has pyqt by default"""
+    #dirname = os.path.dirname(os.path.realpath(__file__))
+    qhroot = get_qhroot()
+    path = qhroot.joinpath("pysrc", "interpreter")
+    sys.path.insert(1, str(path))  # add this path
+    print("Path: ", str(path))
+    import pycommand
+    return pycommand.get_python()
 
 
 python = get_command()
 
 
-if args.python!="": # non empty string
-    f = open(qhroot+"/pysrc/interpreter/pythoninterpreter.py","w")
-    f.write("mainpython = \""+args.python+"\"\n")
-    f.close()
+if args.python != "":  # non empty string
+    path = qhroot.joinpath("pysrc", "interpreter", "pythoninterpreter.py")
+    with open(path, mode='wt') as config:
+        config.write("mainpython = r\""+args.python+"\"\n")    
 
 
+# Use a utility and exit
+if args.utility != "":  # non empty string
+    utility = qhroot.joinpath('utilities')
+    subprocess.run(["python", str(utility), args.utility])
+    #os.system(python + " "+qhroot+"/utilities/"+args.utility)
+    exit()
 
-# use an utility and exit
-if args.utility!="": # non empty string
-  os.system(python + " "+qhroot+"/utilities/"+args.utility)
-  exit()
 
-
-os.system(python+" "+qhroot+"/bin/versions/quantum-honeycomp-pyqt")
+pyqt = qhroot.joinpath('bin', "versions", "quantum-honeycomp-pyqt")
+subprocess.run(["python", str(pyqt)])
